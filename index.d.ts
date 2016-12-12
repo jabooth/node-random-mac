7a8ba020e26329dc5e1252d2944d6494d9053ce4
James Booth <jabooth@gmail.com>
2016-12-12 18:54:58 +0000

    Add typescript definition

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for random-mac 0.0.4
+// Project: node-random-mac
+// Definitions by: James Booth github.com/jabooth
+declare function randomMac(prefix?: string): string;
+declare namespace randomMac {}
+export = randomMac
